15712f6d2174e9092441a16b3b11793727f27058
NatachaVergara <ntchvergara@gmail.com>
2021-06-07 18:35:33 -0300

    arregle css y html

--- js/script.js
+++ js/script.js
@@ -2,14 +2,17 @@
 console.log("Estoy linkeado")
 let btn = document.querySelector(".btnCargar");
 
-let ciudad = document.querySelectorAll("h1")[0];
+// let ciudad = document.querySelectorAll("h1")[0];
 let temperatura = document.querySelector(".temp");
 let humedad = document.querySelector(".humedad");
 let sensacionTermica = document.querySelector(".sTermica");
-let cielo = document.querySelector(".cClimatica");
+let condClimatica = document.querySelector(".cClimatica");
 let iconosVer = document.querySelector(".scroll");
 let icono = document.querySelector(".icono");
 
+
+
+let nomCiudad = document.querySelector(".ciudad");
 
 
 // console.log(temperatura)
@@ -21,7 +24,7 @@ btn.addEventListener("click", peticionClima)
 
 function peticionClima() {
     //Si un usuario deja el input vacio
-    let ciudadInput = (document.querySelector(".ciudad").value);
+    let ciudadInput = document.getElementById("ciudad").value;
     btn.innerHTML = "Volver a buscar";
     
 
@@ -35,14 +38,13 @@ function peticionClima() {
             .then(respuesta => {
 
 
-                console.log(ciudadInput)
-                // ciudad.innerText += `${respuesta.name}`
+                console.log(ciudadInput);
+                nomCiudad.innerText = `${ciudadInput}`;
                 temperatura.innerText = ` ${respuesta.main.temp}°`;
                 humedad.innerText = ` ${respuesta.main.humidity}%`;
                 sensacionTermica.innerText = ` ${respuesta.main.feels_like}°`;
-                cielo.innerText = ` ${respuesta.weather[0].description}`
-                document.getElementById("icon").src = `http://openweathermap.org/img/w/${respuesta.weather[0].icon}.png`;
-
+                condClimatica.innerText = ` ${respuesta.weather[0].description}`
+                
 
                 
                 
@@ -50,10 +52,12 @@ function peticionClima() {
             })
             .catch(err => console.log(err))
            
-        iconosVer.scrollIntoView({ inline: "center", behavior: "smooth" });
+        // iconosVer.scrollIntoView({ inline: "center", behavior: "smooth" });
     }
 
 }
+
+
 
 
 
